5c97bca62cc90222ffd208da34479d476fae12ef
Lukáš Hermann <tuxilero@gmail.com>
2022-03-14 11:23:41 +0100

    Remove sample tests, reformat code, change icon

--- app/src/main/kotlin/com/strv/archdemo/MainActivity.kt
+++ app/src/main/kotlin/com/strv/archdemo/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.strv.archdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.strv.archdemo.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main_activity)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container, MainFragment())
                 .commitNow()
         }
     }
--- app/src/main/kotlin/com/strv/archdemo/ui/main/MainFragment.kt
+++ app/src/main/kotlin/com/strv/archdemo/ui/main/MainFragment.kt
@@ -1,32 +1,23 @@
 package com.strv.archdemo.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.strv.archdemo.R
 
 class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
