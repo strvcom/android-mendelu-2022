76679055dd13ad671ef3a4727ced871bfe0b3221
Lukáš Hermann <tuxilero@gmail.com>
2022-03-30 13:04:56 +0200

    Implementation of ActionBar navigation

--- app/src/main/kotlin/com/strv/archdemo/MainActivity.kt
+++ app/src/main/kotlin/com/strv/archdemo/MainActivity.kt
@@ -2,12 +2,33 @@ package com.strv.archdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBar()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(findViewById(R.id.nav_host_fragment)).navigateUp()
+    }
+
+    // Black box. Not relevant
+    private fun setupActionBar() {
+        val host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        // IDs of fragments you want without the ActionBar home/up button
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.mainFragment))
+
+        setupActionBarWithNavController(host.navController, appBarConfiguration)
     }
 }
